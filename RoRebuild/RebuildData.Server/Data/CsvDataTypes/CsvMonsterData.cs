4b93e2ffd52f0dfad0207c34c54fd4ff5ea0b0e7
Doddler <doddler@gmail.com>
2020-03-03 08:41:05 -0700

    Started work to add monster combat stats.

--- RoRebuild/RebuildData.Server/Data/CsvDataTypes/CsvMonsterData.cs
+++ RoRebuild/RebuildData.Server/Data/CsvDataTypes/CsvMonsterData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RebuildData.Shared.Enum.EntityStats;
 
 namespace RebuildData.Server.Data.CsvDataTypes
 {
@@ -9,7 +10,30 @@ namespace RebuildData.Server.Data.CsvDataTypes
 		public int Id { get; set; }
 		public string Code { get; set; }
 		public string Name { get; set; }
+		public int Level { get; set; }
+		public int HP { get; set; }
+		public int Str { get; set; }
+		public int Int { get; set; }
+		public int Dex { get; set; }
+		public int Agi { get; set; }
+		public int Luk { get; set; }
+		public int AtkMin { get; set; }
+		public int AtkMax { get; set; }
+		public int Range { get; set; }
+		public int Def { get; set; }
+		public int MDef { get; set; }
+		private int Exp { get; set; }
+		public int JExp { get; set; }
+		public int ScanDist { get; set; }
+		public int ChaseDist { get; set; }
+		public CharacterSize Size { get; set; }
+		public CharacterRace Race { get; set; }
+		public CharacterElement Element { get; set; }
+		public int RechargeTime { get; set; }
+		public int AttackTime { get; set; }
+		public int HitTime { get; set; }
 		public int MoveSpeed { get; set; }
+		public CharacterSpecialType Special { get; set; }
 		public string ClientSprite { get; set; }
 		public float ClientOffset { get; set; }
 		public float ClientShadow { get; set; }
